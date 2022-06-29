24bb5d2178643471243645fa27db746fdeffedda
Dan Hsu <hsu86110@gmail.com>
2022-06-29 15:20:47 +0800

    feature: 1. connect Typography component with Theme context 2. delete function fontVariant

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
 import { BasicProps, BasicDivStyled } from '../Basic/Basic'
-import { fontVariant } from '../../utils/fontVariant'
+import createTypography, { Typography, Variant } from '../../core/styles/createTypography'
+
+const defaultTypographyTheme = createTypography({})
 
 interface TypographyStyledProps extends BasicProps {
-  variant?: string
+  typographyStyle: Typography
+  variant?: Variant
   color?: string
   align?: string
   padingTop?: string
@@ -13,26 +16,60 @@ interface TypographyStyledProps extends BasicProps {
   padingLeft?: string
 }
 
-const TypographyStyled = styled(BasicDivStyled)<TypographyStyledProps>`
-  width: ${(props) => (props.width === undefined ? 'auto' : props.width)};
-  height: ${(props) => (props.height === undefined ? '24px' : props.height)};
-  ${(props) => fontVariant(props.variant === undefined ? '' : props.variant)})};
-  color: ${(props) => (props.color === undefined ? props.theme?.textColor?.primary : props.color)};
-  text-align: ${(props) => (props.align === undefined ? 'center' : props.align)};
-  padding-top: ${(props) => (props.padingTop === undefined ? '0' : props.padingTop)};
-  padding-right: ${(props) => (props.padingRight === undefined ? '16px' : props.padingRight)};
-  padding-bottom: ${(props) => (props.padingBottom === undefined ? '0' : props.padingBottom)};
-  padding-left: ${(props) => (props.padingLeft === undefined ? '16px' : props.padingLeft)};
-`
+const TypographyStyled = styled(BasicDivStyled)<TypographyStyledProps>((props) => {
+  if (props.variant === undefined) {
+    return {
+      width: props.width === undefined ? 'auto' : props.width,
+      height: props.height === undefined ? '24px' : props.height,
+      color: props.color === undefined ? props.theme?.textColor?.primary : props.color,
+      textAlign: props.align === undefined ? ('center' as 'center') : (props.align as 'center' | 'right' | 'left'),
+      paddingTop: props.padingTop === undefined ? '0' : props.padingTop,
+      paddingRight: props.padingRight === undefined ? '16px' : props.padingRight,
+      paddingBottom: props.padingBottom === undefined ? '0' : props.padingBottom,
+      paddingLeft: props.padingLeft === undefined ? '16px' : props.padingLeft,
+      fontFamily: props.typographyStyle.fontFamily,
+      fontSize: props.typographyStyle.fontSize,
+      fontWeight: props.typographyStyle.fontWeightLight,
+    }
+  } else {
+    const typographyStyledObject: Record<string, string | number> = {}
+    Object.keys(props.typographyStyle[props.variant]).forEach((key) => {
+      typographyStyledObject[key] = props.typographyStyle[props.variant as Variant][key] as string | number
+    })
+
+    return {
+      width: props.width === undefined ? 'auto' : props.width,
+      height: props.height === undefined ? '24px' : props.height,
+      color: props.color === undefined ? props.theme?.textColor?.primary : props.color,
+      textAlign: props.align === undefined ? ('center' as 'center') : (props.align as 'center' | 'right' | 'left'),
+      paddingTop: props.padingTop === undefined ? '0' : props.padingTop,
+      paddingRight: props.padingRight === undefined ? '16px' : props.padingRight,
+      paddingBottom: props.padingBottom === undefined ? '0' : props.padingBottom,
+      paddingLeft: props.padingLeft === undefined ? '16px' : props.padingLeft,
+      ...typographyStyledObject,
+    }
+  }
+})
 
 export interface TypographyProps extends TypographyStyledProps {
+  themeContext?: React.Context<Typography>
   style?: React.CSSProperties
   children?: React.ReactNode
 }
 
-const Typography = ({ style, children, ...rest }: TypographyProps) => {
+const Typography = ({ style, children, themeContext, ...rest }: TypographyProps) => {
+  if (themeContext) {
+    const theme: Typography = useContext<Typography>(themeContext)
+
+    return (
+      <TypographyStyled {...rest} style={style} typographyStyle={theme}>
+        {children}
+      </TypographyStyled>
+    )
+  }
+
   return (
-    <TypographyStyled {...rest} style={style}>
+    <TypographyStyled {...rest} style={style} typographyStyle={defaultTypographyTheme}>
       {children}
     </TypographyStyled>
   )
